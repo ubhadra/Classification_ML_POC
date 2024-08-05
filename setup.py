c4ec5b856b4a79ae5588f23b6339305d6d7be4d6
ubhadra <bhadra.upahar@gmail.com>
2024-08-06 01:54:25 +0530

    2nd commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+setup(
+    name="ML_POC_Upahar",
+    version="0.0.1",
+    author="Upahar Bhadra",
+    author_email="bhadra.upahar@gmail.com",
+    packages=find_packages()
+)
